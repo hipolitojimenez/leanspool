86b04836874835fb1820ea209553377da617ccba
hipolitojimenez <hipolitojimenez@yahoo.es>
2013-04-17 15:14:21 +0200

    Initial West Region implementation

--- src/main/java/com/nioos/leanspool/gxt/client/LeanSpooler.java
+++ src/main/java/com/nioos/leanspool/gxt/client/LeanSpooler.java
@@ -4,6 +4,10 @@ package com.nioos.leanspool.gxt.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.sencha.gxt.core.client.util.Margins;
+import com.sencha.gxt.widget.core.client.ContentPanel;
+import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer.BorderLayoutData;
 import com.sencha.gxt.widget.core.client.container.Viewport;
 
 
@@ -14,6 +18,22 @@ public class LeanSpooler implements EntryPoint {
 	@Override
 	public void onModuleLoad() {
 		Viewport viewport = new Viewport();
+		//
+		BorderLayoutContainer borderLayoutContainer =
+			new BorderLayoutContainer();
+		//
+		BorderLayoutData westWidgetLayoutData = new BorderLayoutData(200);
+		westWidgetLayoutData.setCollapsible(true);
+		westWidgetLayoutData.setSplit(true);
+		Margins westMargins = new Margins(1);
+		westWidgetLayoutData.setMargins(westMargins);
+		ContentPanel westWidget = new ContentPanel();
+		borderLayoutContainer.setWestWidget(westWidget, westWidgetLayoutData);
+		//
+		ContentPanel centerWidget = new ContentPanel();
+		borderLayoutContainer.setCenterWidget(centerWidget);
+		//
+		viewport.add(borderLayoutContainer);
 		//TODO
 		RootPanel.get().add(viewport);
 	}
